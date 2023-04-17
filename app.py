8af2159d2cfeb1dba1731f72528eebacff50e78a
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-17 11:14:02 +0530

    Methodology done

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ def load_about():
     st.subheader('Objective')
     st.write('The ultimate objective of CVD detection is to identify the disease as possible, prevent or manage its progression, and improve the overall health and well early as being of patients. By using various diagnostic tests and technologies, healthcare providers aim to provide personalized and effective treatment plans to patients, with the goal of reducing the incidence of CVD and its associated complications.')
 
+    st.subheader('Methodology')
+    st.markdown('#### Dataset')
+    st.write('This project uses the Cardiovascular Disease dataset from Kaggle which contains 13 attributes and samples.')
+
+    st.markdown('#### Classification Models')
+    st.write('To improve the accuracy of cardiovascular disease diagnosis, we will propose an ensemble model based on machine learning algorithms, including XGBoost, K Nearest Neighbours, and Decision Trees, as well as a stacked model that combines an ML ensemble model with a Random Forest.We will utilize four machine learning techniques, namely Random Forest, KNN, Decision Trees, and XGBoost, in our project.')
+
 
 st.sidebar.header('Cardiovascular Disease Detection Using Python')
 option = st.sidebar.radio('Choose', ['About', 'Try the model'])
