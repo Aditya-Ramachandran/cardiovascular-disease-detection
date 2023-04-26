d0513d81153c7a5272fd4b473616371f814a6d9b
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-26 13:00:22 +0530

    Coded filtering for rest of the operators

--- explore.py
+++ explore.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('Dataset/cardio_train_cleaned_1.csv')
+df['age'] = round(df['age'])
 
 # col1,col2,col3 = st.columns(3)
 
 class Explore():
 
     def filter(self):
-        explore_col =  st.selectbox('Select Column', [cols for cols in df.columns])
+        explore_col =  st.selectbox('Select Column', [cols for cols in df.columns][1:])
         explore_operator = st.selectbox('Select Operator',['>', '>=','<','<=','=='])
         explore_num = st.number_input('Enter the value based on which you want to filter')
         self.__do_filter(df, explore_col, explore_operator, explore_num)
@@ -21,9 +22,18 @@ class Explore():
     def __do_filter(self,dataframe_name, col_name, operator, number):
         if operator == '>':
             filtered_df = dataframe_name[dataframe_name[col_name] > number]
+        
+        if operator == '>=':
+            filtered_df = dataframe_name[dataframe_name[col_name] >= number]
 
+        if operator == '<':
+            filtered_df = dataframe_name[dataframe_name[col_name] < number]
 
+        if operator == '<=':
+            filtered_df = dataframe_name[dataframe_name[col_name] <= number]
 
+        if operator == '==':
+            filtered_df = dataframe_name[dataframe_name[col_name] == number]
 
         col1, col2, col3 = st.columns(3)
         with col1:
